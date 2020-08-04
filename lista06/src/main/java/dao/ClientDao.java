a01f6fa4c5670ca6b96e8b35e1003f8b0df50632
Hevelson <hevelson@gmail.com>
2020-08-03 21:23:29 -0300

    Compilado projeto lista 06

--- lista06/src/main/java/dao/ClientDao.java
+++ lista06/src/main/java/dao/ClientDao.java
@@ -12,18 +12,18 @@ import util.DatabaseConnection;
 
 public class ClientDao {
     
-    private Connection con;
+    private final Connection con;
 
     public ClientDao() {
         this.con = new DatabaseConnection().getConnection();
     }
 
-    public void create(Client client) throws SQLException {
+    public void create(final Client client) throws SQLException {
 
         String query;
         query = "INSERT INTO client (name, address, phone, cep) VALUES (?, ?, ?, ?);";
 
-        PreparedStatement st = con.prepareStatement(query);
+        final PreparedStatement st = con.prepareStatement(query);
         st.setString(1, client.getName());
         st.setString(2, client.getAddress());
         st.setString(3, client.getPhone());
@@ -35,16 +35,16 @@ public class ClientDao {
     }
 
     public List<Client> list() throws SQLException, Exception {
-        
-        List<Client> list = new ArrayList();
-        String query = "SELECT * FROM client";
-        
-        PreparedStatement st = con.prepareStatement(query);
-        
-        ResultSet rs = st.executeQuery();
-        
-        while(rs.next()) {            
-            Client client = new Client();
+
+        final List<Client> list = new ArrayList();
+        final String query = "SELECT * FROM client";
+
+        final PreparedStatement st = con.prepareStatement(query);
+
+        final ResultSet rs = st.executeQuery();
+
+        while (rs.next()) {
+            final Client client = new Client();
 
             client.setIdClient( rs.getInt("id") );
             client.setName( rs.getString("name") );
